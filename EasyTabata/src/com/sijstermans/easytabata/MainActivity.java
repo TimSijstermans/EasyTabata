5c23038379712ab954b0b58527aaf5e3a4fba280
TimSijstermans <timpie-s@hotmail.com>
2014-03-31 02:03:50 +0200

    Got started, added basic layout and test ads

--- EasyTabata/src/com/sijstermans/easytabata/MainActivity.java
+++ EasyTabata/src/com/sijstermans/easytabata/MainActivity.java
@@ -1,29 +1,65 @@
 package com.sijstermans.easytabata;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends ActionBarActivity {
 
+	private AdView adView;
+	
+	private static final String AD_UNIT_ID = "ca-app-pub-1863889756478321/8692970496";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
+	
+	@Override
+	protected void onStart(){
+		super.onStart();
+		// Create an ad.
+	    adView = new AdView(this);
+	    adView.setAdSize(AdSize.BANNER);
+	    adView.setAdUnitId(AD_UNIT_ID);
 
+	    // Add the AdView to the view hierarchy. The view will have no size
+	    // until the ad is loaded.
+	    ViewGroup layout = (ViewGroup) findViewById(R.id.mainLayout);
+	    
+	    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+	    params.addRule(RelativeLayout.BELOW, R.id.exerciseCounter);
+	    adView.setLayoutParams(params);
+	    layout.addView(adView);
+
+	    // Create an ad request. Check logcat output for the hashed device ID to
+	    // get test ads on a physical device.
+	    AdRequest adRequest = new AdRequest.Builder()
+	        .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+	        .addTestDevice("4df167ff01c79f97")
+	        .build();
+
+	    // Start loading the ad in the background.
+	    adView.loadAd(adRequest);
+	
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
@@ -60,5 +96,18 @@ public class MainActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	
+	private void setupStrings(){
+		TextView txtView = (TextView) findViewById(R.id.counter);
+		txtView.setText("00:00");
+		txtView = (TextView) findViewById(R.id.exerciseCounter);
+		txtView.setText("exercise: 0 / 5");
+	}
+	
+	public void startTabata(View v){
+		System.out.println("test");
 
+	}
+	
+	
 }
